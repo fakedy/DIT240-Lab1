6f380d159a3972616a74a53e893c8eb04a495534
fakedy <34602297+fakedy@users.noreply.github.com>
2025-11-01 17:27:00 +0100

    Start of go http server

--- http_server.go
+++ http_server.go
@@ -6,4 +6,33 @@ import("net")
 
 func main() {
 	fmt.Println("Hello World!")
+
+	// Create listener
+	// Go support multiple return falues thus the following is valid
+
+	listener, err := net.Listen("tcp", ":8080")
+
+	if(err != nil){	// err will not be nil if there is an error
+		fmt.Println("Failed to create listener")
+	} else {
+		addr := listener.Addr()
+		fmt.Printf("Listening to address: %s\n", addr)
+	}
+
+	// continiously check for connections
+	for{
+		connection, err := listener.Accept()
+		if(err != nil){
+			fmt.Println("Failed to accept connection")
+		} else {
+			fmt.Printf("Connection established with: %s\n", connection.RemoteAddr())
+		}
+
+		// temporary
+		connection.Close()
+
+	}
+
+	listener.Close()
+
 }
